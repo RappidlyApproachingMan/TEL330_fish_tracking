4cb18d4a6be8d8155e52a344cb31b34de776dfa7
Daniel <dam@lem.org>
2026-04-24 10:20:30 +0200

    Add initial implementation of straight line movement controller

---
+++ straight_line.py
@@ -0,0 +1,32 @@
+
+# Figuring out how to use ur-rtde by having robot end effector move in a straight line from a to b
+
+import rtde_control
+import rtde_receive
+import cv2
+
+class controller():
+    def __init__(self):
+        # set up openCV window to use it's keyboard functions
+
+
+        pass
+
+    def move(self, start, end):
+        # rtde controls
+
+
+
+        # opencv buttons for:
+        #   start
+        #   stop
+        #   close everything
+        pass
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
